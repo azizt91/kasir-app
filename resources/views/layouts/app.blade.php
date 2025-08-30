c7b970961c85224017b1e4a8020a10f46dfd109b
azizt91 <21195038@mhs.stmik-tegal.ac.id>
2025-08-30 18:35:51 +0700

    tambah folder api

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
         </div>
 
         <!-- Mobile Sidebar Overlay -->
-        <div x-show="sidebarOpen" 
+        <div x-show="sidebarOpen"
              x-transition:enter="transition-opacity ease-linear duration-300"
              x-transition:enter-start="opacity-0"
              x-transition:enter-end="opacity-100"
